0e65f048d3646c06630df020857f22507dc1b09c
gali1917 <carlgale93@hotmail.com>
2022-04-12 14:46:55 -0500

    Feature/Gali/Animacion de cover

---
+++ src/animation.js
@@ -0,0 +1,26 @@
+const ul = document.getElementById('listCover');
+
+function frame(){
+    const animation = ul.animate([
+        {transform: "TranslateY(0px)"},
+        {transform: "TranslateY(-24px)"}
+    ], {
+        easing: "Linear",
+        iterations: 1,
+        duration: 200
+    });
+
+    return animation.finished;
+}
+
+function disp(){
+    frame()
+    .then((resolve) =>{
+        console.log(resolve);
+        ul.appendChild(document.getElementsByClassName("list" )[0])
+    })
+}
+
+setInterval(() => {
+    disp();
+}, 1000);

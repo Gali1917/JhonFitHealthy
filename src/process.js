577a621d227ff65c0baddfc5989c90a372140e4c
gali1917 <carlgale93@hotmail.com>
2022-04-16 18:59:26 -0500

    Feature/Gali/Animation

--- src/process.js
+++ src/process.js
@@ -1,19 +1,52 @@
-function buttonCard(){
-    const height = window.innerHeight - 280;
+var buttonPosition = 0;
+
+function buttonCardOpen(){
     gsap.to('.article', {
         duration: 0.5,
-        y: -height,
+        y: -360,
         delay: 0
     });
+    buttonPosition = 1;
+    cardsAnimation();
 }
+
+function buttonCardClose(){
+    gsap.to('.article', {
+        duration: 0.5,
+        y: 0,
+        delay: 0
+    });
+    buttonPosition = 0;
+
+}
+
+function cardsAnimation(){
+    for(x = 1; x <= 5; x++){
+        var text = '.cards' + x
+    }
+    gsap.from('.cards', {
+        duration: 0.5,
+        x: -500,
+        delay: 0.3
+    })
+}
+
 
 function buttonDown(){
     const cards = document.getElementById('cardsId');
     const headerArticle = document.getElementById('headerArticle');
     const imgButtonId = document.getElementById('imgButtonId');
-    imgButtonId.style.transform = 'rotate(180deg)';
-    buttonCard();
-    headerArticle.style.overflow = 'hidden';
-    cards.style.display = 'block';
+    if(buttonPosition == 0){
+        imgButtonId.style.transform = 'rotate(180deg)';
+        buttonCardOpen();
+        headerArticle.style.overflow = 'hidden';
+        cards.style.display = 'block';
+    }
+    else{
+        imgButtonId.style.transform = 'rotate(0deg)';
+        buttonCardClose();
+        headerArticle.style.overflow = 'hidden';
+        cards.style.display = 'none';
+    }
 
 }
